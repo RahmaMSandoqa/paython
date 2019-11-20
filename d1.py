1315b1663c41d3e2c0a3651f96dd320b7a93dc7e
RahmaMSandoqa <rahma.san.m@gmail.com>
2019-11-20 16:36:23 +0200

    new ex2

---
+++ d1.py
@@ -0,0 +1,187 @@
+#Rahma = "hello rahma"
+#print ("Rahman\bmaher")
+
+#------------------------------------------------------------------
+#age = int(input("what is your Age?"))
+#print (age)
+#print (type(age))
+#x, y, z = "rahma","lojen","rashed"
+#print(x)
+#print(y)
+#print(z)
+#-----------------------------------------------------------------
+#DAY 2
+
+#a,b = 1,10
+#if a >b:
+#    print("a > b")
+#elif a <b:
+#    print("a < b")
+#else:
+#    print("a = b")
+
+
+#
+#a,b,c=1,10,55
+#max = a if (a > b > c) else c 
+#print(max)
+#
+#-----------------------------------------------------------
+#if 'rahma' in ['ahmad','rahma','hamzeh']:
+#    print('rahma in a list')
+#else: print('rahma not ')
+#
+#a =100
+#b =330
+#print ('a') if a > b else print ("=") if a == b else print("b")
+
+#
+#a= int(input("inter number"))
+#b= int(input("inter number"))
+#if ( a > b ):
+#    print( b ,a)
+#else:
+#    print( a, b)
+
+
+#-----------------------------------------------------------------
+#name = (input("enter your name"))
+#age = int(input("enter your age"))
+#
+#if ( age < 18 ):
+#    print("under age ")
+#    Average= int(input("enter your Average"))
+#    if(Average >= 90):
+#        print("Excellent Average")
+#    elif(Average >= 50 >90):
+#        print("passed")
+#    else:
+#        print("failed")
+#    
+#    
+#else:
+#    job =(input("what your job?"))
+#    print("age,job")
+#------------------------------------------------------------------
+#
+#for rahma in range(9):
+#    print(rahma)
+#---------------------------------
+#for a in range(0,6,9,6,2):
+#    print(a)
+#---------------------------------
+#for x in "name":
+#    print(x)
+#====================================
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#    print(x)
+#    if x == "banana":
+#        break
+##------------------------------------
+#
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#    if x == "banana":
+#        continue
+#    print(x)
+
+#---------------------------------------------
+#
+#for x in range(6):
+#    print(x)
+#else:
+#    print("Finally finished!")
+#--------------------------------------------------
+#while True:
+#    a=input('>')
+#    if a=='exit':
+#        break
+#    print (a)
+#---------------------------------------------------
+#i =0
+#while i<10:
+#    print("*")
+#    i+=1
+
+#----------------------------------------------------
+#i =0
+#while i<31:
+#    print("*" , end='')
+#    i+=1
+#---------------------------------------------------
+
+
+for x in range(10):
+    for i in range(x):
+       print("*",end="")
+    print()
+
+#-------------------------------------------------
+#for x in range(8):
+#    for i in range(x):
+#       print("",end="")
+#       X+=1
+#    for i in range(x):
+#       print("*",)
+#    print()
+#
+#while True:
+#     try:
+#         n = input("Please enter an integer: ")
+#         n = int(n)
+#         break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
